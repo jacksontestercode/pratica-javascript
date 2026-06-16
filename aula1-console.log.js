cb6d516faaf640e9ca393811461cad90ebf5faa1
Jackson Mendes <jacksonfla6@gmail.com>
2026-06-16 19:50:58 -0300

    feat: adiciona prática de console.log

---
+++ aula1-console.log.js
@@ -0,0 +1,5 @@
+console.log("'Jackson Mendes'"); // String = Texto
+console.log('"Jackson Mendes"'); // Number = Número
+console.log('Jackson Mende');
+
+console.log(15, 85.5, 'Jackson Mendes Novo');
