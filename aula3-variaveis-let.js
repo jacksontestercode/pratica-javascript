0f9539a46d5ff74fdd4b9a3e3ae2201be96c6d50
Jackson Mendes <jacksonfla6@gmail.com>
2026-06-17 14:05:46 -0300

    feat: adiciona prática variáveis let

---
+++ aula3-variaveis-let.js
@@ -0,0 +1,26 @@
+// let nome = 'Paulo'; // String
+
+// console.log(nome, 'nasceu em 1977.');
+// console.log('Em 2000', nome, 'conheceu Maria.');
+// console.log(nome, 'casou-se com Maria em 2012.');
+// console.log('Maria teve 1 filho com', nome, 'em 2015.');
+// console.log('O filho de', nome, 'se chama Eduardo.');
+
+// let nome; // Declarou a variável
+// nome = 'Jackson'; // Inicializando a variável
+// console.log(nome);
+// nome = 'Mendes';
+// console.log(nome);
+
+// Não pode redeclarar a mesma variável let. Erro para a execução do códgio
+
+// let nome;
+
+// Não podemos criar variáveis com palavras reservada
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclar variáveis com let
+// NÃO UTILIZE VAR, UTILIZE LET
