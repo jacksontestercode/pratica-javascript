a72f5ef64586e265320e950b87bd5148bb0bc085
Jackson Mendes <jacksonfla6@gmail.com>
2026-06-17 09:44:39 -0300

    feat: adiciona prática de comentários

---
+++ aula2-comentarios.js
@@ -0,0 +1,13 @@
+// E escrever um comentário longo
+console.log('Olá mundo!'); // Aqui tem outro comentário
+console.log('Lina 1');
+// Comentário
+console.log('Lina 2');
+
+/* 
+Um bloco de código
+Linha de comentário
+*/
+console.log('Lina 3');
+console.log('Lina 4');
+// Vai ser ignorado
